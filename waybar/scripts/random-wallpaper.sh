75e1e456b4a410123fb482e0e0a493366f07ded5
prince <prince.babariya@trinary.in>
2025-06-28 21:33:10 +0530

    [ADD] add configs

---
+++ waybar/scripts/random-wallpaper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Directory containing wallpapers
+WALLPAPER_DIR="$HOME/Pictures/wallpaper"
+
+# Get random wallpaper
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
+
+if [ -z "$WALLPAPER" ]; then
+    echo "No wallpapers found in $WALLPAPER_DIR"
+    exit 1
+fi
+
+echo "Setting wallpaper: $WALLPAPER"
+
+# Preload the wallpaper first
+hyprctl hyprpaper preload "$WALLPAPER"
+
+# Then set it as wallpaper
+hyprctl hyprpaper wallpaper ",$WALLPAPER"
