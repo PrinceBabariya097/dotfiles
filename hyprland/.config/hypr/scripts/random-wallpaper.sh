d97cec2f85babb698dbe03e2ce8e0a1f297a1ab7
PrinceBabariya097 <prince.babariya.786@gmail.com>
2025-07-01 23:03:00 +0530

    [ADD] add theme, hyprlock, fix stow, add styles for wofi

--- hyprland/.config/hypr/scripts/random-wallpaper.sh
+++ hyprland/.config/hypr/scripts/random-wallpaper.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 # Directory containing wallpapers
 WALLPAPER_DIR="$HOME/Pictures/wallpaper"
 
-# Get random wallpaper
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
+# Get random wallpaper (follow symlinks with -L)
+WALLPAPER=$(find -L "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
 
 if [ -z "$WALLPAPER" ]; then
     echo "No wallpapers found in $WALLPAPER_DIR"
@@ -12,9 +11,7 @@ if [ -z "$WALLPAPER" ]; then
 fi
 
 echo "Setting wallpaper: $WALLPAPER"
-
 # Preload the wallpaper first
 hyprctl hyprpaper preload "$WALLPAPER"
-
 # Then set it as wallpaper
 hyprctl hyprpaper wallpaper ",$WALLPAPER"
